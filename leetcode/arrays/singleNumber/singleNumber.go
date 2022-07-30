9d4b89dc2da22bd017c3a19c5f75576f30a4d0ba
Artem Rybakov <gizmo3000@yandex.ru>
2022-07-30 15:28:26 +0300

    +singleNumber search

---
+++ leetcode/arrays/singleNumber/singleNumber.go
@@ -0,0 +1,31 @@
+package leetcode
+
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+// Example 3:
+
+// Input: nums = [1]
+// Output: 1
+
+func singleNumber(nums []int) int {
+	m := make(map[int]int, len(nums))
+	for i := range nums {
+		m[nums[i]]++
+	}
+	for k, v := range m {
+		if v == 1 {
+			return k
+		}
+	}
+	return 0
+}
