a451875d487a70b1846ea694522a7e87f844f877
Artem Rybakov <gizmo3000@yandex.ru>
2022-08-26 11:12:11 +0300

    added searchInsertPosition()

---
+++ leetcode/arrays/searchInsertPosition/searchInsertPosition_test.go
@@ -0,0 +1,43 @@
+package leetcode
+
+import "testing"
+
+func Test_searchInsert(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "[1,3,5,6], target = 5",
+			args: args{nums: []int{1, 3, 5, 6}, target: 5},
+			want: 2,
+		},
+		{
+			name: "[1,3,5,6], target = 2",
+			args: args{nums: []int{1, 3, 5, 6}, target: 2},
+			want: 1,
+		},
+		{
+			name: "[1,3,5,6], target = 7",
+			args: args{nums: []int{1, 3, 5, 6}, target: 7},
+			want: 4,
+		},
+		{
+			name: "[1,3,5], target = 4",
+			args: args{nums: []int{1, 3, 5}, target: 4},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchInsert(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInsert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
