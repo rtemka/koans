e608008e3f801a8172e84f5102e0878ac705c7fa
Artem Rybakov <gizmo3000@yandex.ru>
2022-09-01 15:02:06 +0300

    modified searchInsertPosition() solution

--- arrays/searchInsertPosition/searchInsertPosition.go
+++ arrays/searchInsertPosition/searchInsertPosition.go
@@ -1,6 +1,7 @@
 package leetcode
 
-// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// Given a sorted array of distinct integers and a target value, return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
 
 // You must write an algorithm with O(log n) runtime complexity.
 
@@ -40,5 +41,30 @@ func searchInsert(nums []int, target int) int {
 		return mid + 1
 	} else {
 		return mid
+	}
+}
+
+func SearchInsert(nums []int, target int) int {
+
+	lo := 0
+	hi := len(nums) - 1
+
+	for lo < hi {
+		mid := (lo + hi) / 2
+
+		diff := target - nums[mid]
+		if diff < 0 {
+			hi = mid - 1
+		} else if diff > 0 {
+			lo = mid + 1
+		} else {
+			return mid
+		}
+	}
+
+	if target > nums[lo] {
+		return lo + 1
+	} else {
+		return lo
 	}
 }
--- arrays/searchInsertPosition/searchInsertPosition_test.go
+++ arrays/searchInsertPosition/searchInsertPosition_test.go
@@ -41,3 +41,48 @@ func Test_searchInsert(t *testing.T) {
 		})
 	}
 }
+
+func TestSearchInsert(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "[1,3,5,6], target = 5",
+			args: args{nums: []int{1, 3, 5, 6}, target: 5},
+			want: 2,
+		},
+		{
+			name: "[1,3,5,6], target = 2",
+			args: args{nums: []int{1, 3, 5, 6}, target: 2},
+			want: 1,
+		},
+		{
+			name: "[1,3,5,6], target = 7",
+			args: args{nums: []int{1, 3, 5, 6}, target: 7},
+			want: 4,
+		},
+		{
+			name: "[1,3,5], target = 4",
+			args: args{nums: []int{1, 3, 5}, target: 4},
+			want: 2,
+		},
+		{
+			name: "[1,3,5], target = -1",
+			args: args{nums: []int{1, 3, 5}, target: -1},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SearchInsert(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInsert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
