8446b6a39d2c705c8cb09be350b78be0e6929bf2
rtemka <rybakov333@gmail.com>
2025-05-17 22:53:57 +0300

    27. remove element //again

--- arrays/removeElement/removeElement.go
+++ arrays/removeElement/removeElement.go
@@ -1,18 +1,17 @@
+// #27
+// https://leetcode.com/problems/remove-element/description/
 package leetcode
 
-// Given an integer array nums and an integer val, remove
-// all occurrences of val in nums in-place. The relative order of the elements may be changed.
-
-// Since it is impossible to change the length of the array in some languages,
-// you must instead have the result be placed in the first part of the array nums.
-// More formally, if there are k elements after removing the duplicates,
-// then the first k elements of nums should hold the final result.
-// It does not matter what you leave beyond the first k elements.
-
-// Return k after placing the final result in the first k slots of nums.
-
-// Do not allocate extra space for another array. You must do this
-// by modifying the input array in-place with O(1) extra memory.
+func removeElement(nums []int, val int) int {
+	n := 0
+	for i := range nums {
+		if nums[i] != val {
+			nums[n] = nums[i]
+			n++
+		}
+	}
+	return n
+}
 
 func removeElementNaive(nums []int, val int) int {
 	k := len(nums)
@@ -24,17 +23,4 @@ func removeElementNaive(nums []int, val int) int {
 		}
 	}
 	return k
-}
-
-func removeElement(nums []int, val int) int {
-	var j int = 0
-
-	for i, n := range nums {
-		if n != val {
-			nums[j] = nums[i]
-			j++
-		}
-	}
-
-	return j
 }
