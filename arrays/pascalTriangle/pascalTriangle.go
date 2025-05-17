2cdb7cd466dc0d762a758cd7455de4069370294e
rtemka <rybakov333@gmail.com>
2025-05-17 22:53:28 +0300

    118. pascal triangle //again

--- arrays/pascalTriangle/pascalTriangle.go
+++ arrays/pascalTriangle/pascalTriangle.go
@@ -1,19 +1,31 @@
+// #118
+// https://leetcode.com/problems/pascals-triangle/description/
 package leetcode
 
-// Given an integer numRows, return the first numRows of Pascal's triangle.
-
-// In Pascal's triangle, each number is the sum of the two numbers directly above it.
-
-// Example 1:
-
-// Input: numRows = 5
-// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
-// Example 2:
-
-// Input: numRows = 1
-// Output: [[1]]
-
 func generate(numRows int) [][]int {
+	res := make([][]int, 0, numRows)
+	res = append(res, []int{1})
+	if numRows == 1 {
+		return res
+	}
+	res = append(res, []int{1, 1})
+
+	for i := 1; i < numRows-1; i++ {
+		row := make([]int, 0, len(res[i])+1)
+		// Add one to start.
+		row = append(row, 1)
+		for k := 1; k < len(res[i])-1; k++ {
+			row = append(row, res[i][k]+res[i][k+1])
+		}
+		// Add one to the end
+		row = append(row, 1)
+		res = append(res, row)
+	}
+
+	return res
+}
+
+func GenerateOld(numRows int) [][]int {
 
 	if numRows == 0 {
 		return [][]int{}
